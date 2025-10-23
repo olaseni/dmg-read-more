f1b61c85f7ad4b5541e9e074c1696f130a971bfb
olaseni <277661+olaseni@users.noreply.github.com>
2025-10-23 14:35:21 +0100

    Add plugin boilerplate, and wp-env

---
+++ wp-search-posts-plugin.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Plugin Name:       WP Search Posts
+ * Plugin URI:        https://github.com/olaseni/wp-search-posts
+ * Description:       A WP plugin that adds a Gutenberg block and a wp-cli command for searching posts
+ * Version:           1.0.0
+ * Requires at least: 5.8
+ * Requires PHP:      7.4
+ * Author:            Olaseni Oluwunmi
+ * Author URI:        https://github.com/olaseni
+ * Text Domain:       wp-search-posts
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ */
+define( 'WP_SEARCH_POSTS_VERSION', '1.0.0' );
+
+/**
+ * Plugin initialization.
+ */
+function wp_search_posts_init() {
+	// Add your plugin initialization code here
+}
+add_action( 'plugins_loaded', 'wp_search_posts_init' );
+
+/**
+ * Activation hook.
+ */
+function wp_search_posts_activate() {
+	// Add activation tasks here
+	flush_rewrite_rules();
+}
+register_activation_hook( __FILE__, 'wp_search_posts_activate' );
+
+/**
+ * Deactivation hook.
+ */
+function wp_search_posts_deactivate() {
+	// Add deactivation tasks here
+	flush_rewrite_rules();
+}
+register_deactivation_hook( __FILE__, 'wp_search_posts_deactivate' );
